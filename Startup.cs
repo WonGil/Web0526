3fe234a56e00f65f59558d0974129a0cd5f1e83b
WonGil <wongilgom@gmail.com>
2021-05-26 14:38:30 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace _0526
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!!!!!");
+                    await context.Response.WriteAsync("Hello World!! Hello! Bye! Hi!");
                 });
             });
         }
